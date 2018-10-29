12b61f467f5f0d0498f8c2a7889da3b74b3897ee
nabehide <iasoyv@gmail.com>
2018-10-30 00:05:33 +0900

    fix: travis

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,8 +2,8 @@
   <nav>
     <ul>
       <nuxt-link exact to="/" ><li>Top</li></nuxt-link>
-      <nuxt-link to="/wallpaper" id="wallpaper" ><li>Wallpaper</li></nuxt-link>
-      <nuxt-link to="/demo" id="demo" ><li>Demo</li></nuxt-link>
+      <nuxt-link to="/wallpaper" ><li>Wallpaper</li></nuxt-link>
+      <nuxt-link to="/demo" ><li>Demo</li></nuxt-link>
     </ul>
   </nav>
 </template>
@@ -19,34 +19,19 @@ nav {
     list-style: none;
     margin: 5px 0px;
 
-    border-bottom: none;
+    border-bottom: white solid thin;
 
     .selected {
       border-bottom: black solid thin;
-      -webkit-transition: all 300ms 0s ease-;
-      transition: border-bottom-color 1000ms 0s ease;
+    }
+
+    a {
+      color: black;
     }
 
     li {
       display: inline-block;
       margin: 0px 10px;
-    }
-    a,
-    a:visited {
-      color: black;
-      text-decoration: none;
-    }
-    a:hover {
-      color: black;
-      text-decoration {
-        line: underline;
-        color: black;
-        style: solid;
-      }
-    }
-    a:active {
-      color: black;
-      text-decoration: none;
     }
   }
   border-bottom: {

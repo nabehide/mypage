250316416afe7c6668329349edf6ed2fd9d92ff8
nabehide <iasoyv@gmail.com>
2018-11-03 03:13:53 +0900

    fix: ga

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,8 +1,5 @@
 export default ({ app }) => {
   if (process.env.DEPLOY_ENV !== 'GH_PAGES') return
-  else {
-    console.log("production")
-  }
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -11,7 +8,7 @@ export default ({ app }) => {
 
   ga('create', 'UA-125972611-2' , 'auto')
 
-  approuter.afterEach((to, from) => {
+  app.router.afterEach((to, from) => {
     ga('set', 'page', to.fullPath)
     ga('send', 'pageview')
   })
